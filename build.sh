58f6442b9afe600e35aa87488ac3a4379cfd7de5
Ricky Zhang <rickyzhang@gmail.com>
2018-06-02 19:34:19 -0400

    Add map demo
    Signed-off-by: Ricky Zhang <rickyzhang@gmail.com>

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -std=c++11 container-element-test.cpp -o container-element-test
+g++ -std=c++11 container-element-test.cpp -o container-element-test.o
--- container-element-test.cpp
+++ container-element-test.cpp
@@ -1,29 +1,66 @@
 #include <vector>
+#include <map>
 #include <iostream>
 #include <memory>
 
 using namespace std;
-
+const int NUM_OBJ=3;
 class A
 {
 public:
-    A(){cout << "constructor" << endl;};
-    ~A(){cout << "destructor"  << endl;};
+    A(int index): index(index) {cout<< "constructor of A with index: "<<index<< endl;};
+    ~A(){cout<<"destructoro of A with index:"<<index<< endl;};
+private:
+    int index;
 };
 
-shared_ptr<vector<shared_ptr<A>>> getVector()
+typedef shared_ptr<A> SP_A;
+
+shared_ptr<vector<SP_A>> getVector()
 {
-    auto v = make_shared<vector<shared_ptr<A>>>();
-    for(int i = 0; i < 10; i++){
-        auto element = make_shared<A>();
+    auto v = make_shared<vector<SP_A>>();
+    cout<<"creating "<<NUM_OBJ<<" into vector"<<endl;
+    for(int i = 0; i < NUM_OBJ; i++){
+        auto element = make_shared<A>(i);
         v->push_back(element);
     }
-    cout << "leave getVector()";
+    cout << "leaving getVector()"<<endl;
     return v;
 }
-int main( )
+
+shared_ptr<map<int, SP_A>> getMap()
 {
+    auto m = make_shared<map<int, SP_A>>();
+    cout<<"creating "<<NUM_OBJ<<" into map"<<endl;
+    for(int i = 0; i < NUM_OBJ; i++){
+        auto element = make_shared<A>(i);
+        m->insert(pair<int, SP_A>(i, element));
+    }
+    cout << "leaving getMap()"<<endl;
+    return m;
+
+}
+
+void demoVector()
+{
+    cout<<"starting demo vector"<<endl;
     auto v = getVector();
     v->clear();
-    cout << "I am here" << endl;
+    cout << "finished demo vector" <<endl<<endl;
+}
+
+void demoMap()
+{
+    cout<<"starting demo map"<<endl;
+    auto m = getMap();
+    m->clear();
+    cout << "finished demo map" <<endl<<endl;
+}
+
+int main( )
+{
+    demoVector();
+    demoMap();
+    cout<<"I'm done."<<endl;
+    return 0;
 }
